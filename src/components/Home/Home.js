32aebff758095f844b12267181eb966779ffb498
Ivan Barrios <ivanbar8112003@gmail.com>
2023-02-27 12:21:20 -0300

    Posts loading randomly in Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,7 @@ import Stories from "./Stories";
 import Post from "./Post";
 import AuthDisplay from "./AuthDisplay";
 
-const Home = ({ loggedIn, setLoggedIn, signInWithGoogle, logOut }) => {
+const Home = ({ loggedIn, signInWithGoogle, logOut }) => {
   return (
     <div className="flex w-full justify-center">
       <div className="flex gap-4">
--- src/components/Home/Post.js
+++ src/components/Home/Post.js
@@ -1,28 +1,64 @@
-const PostInfo = () => {
+import { ref, listAll, getDownloadURL } from "firebase/storage";
+import { storage, db } from "../firebase/config";
+import { getDocs, collection } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { async } from "@firebase/util";
+
+const PostInfo = ({ url }) => {
   return (
     <div className="h-fit w-full bg-blue-500">
       <div className="">ProfileInfo</div>
-      <div className="h-52 bg-black text-white">Photo</div>
-      <div>Post Options</div>
+      <div className="flex h-[500px] justify-center text-white">
+        <img className="h-[500px]" src={url} alt="Post" />
+      </div>
     </div>
   );
 };
 
 const Post = () => {
+  const [imageList, setImageList] = useState([]);
+
+  //To get all users email
+  const userInfoCollection = collection(db, "users");
+  const getUserInfo = async () => {
+    //Read data
+    try {
+      const data = await getDocs(userInfoCollection);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      return filteredData;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    async function getPosts() {
+      const usersList = await getUserInfo();
+      const usersEmails = usersList.map((user) => user.email);
+      setImageList([]);
+      usersEmails.forEach((email) => {
+        const imageListRef = ref(storage, `images/${email}`);
+        listAll(imageListRef).then((response) => {
+          response.items.forEach((item) => {
+            getDownloadURL(item).then((url) => {
+              setImageList((prev) => [...prev, url]);
+            });
+          });
+        });
+      });
+    }
+    getPosts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="flex flex-col items-center gap-10">
-      <PostInfo />
-      <PostInfo />
-      <PostInfo />
-      <PostInfo />
-      <PostInfo />
-      <PostInfo />
-      <PostInfo />
-      <PostInfo />
-      <PostInfo />
-      <PostInfo />
-      <PostInfo />
-      <PostInfo />
+      {imageList.map((url, index) => (
+        <PostInfo key={index} url={url} />
+      ))}
     </div>
   );
 };
